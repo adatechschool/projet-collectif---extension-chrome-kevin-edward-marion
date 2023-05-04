443b34247bec202807cf3d5b954eb1c53620145c
Kévin C <kevin.chron@gmail.com>
2023-05-04 15:43:57 +0200

    0.2.0 Amélioration de la fonction pour surligner les mots. Ne prends pas en compte les balise a et leurs enfants. Correction bug tout en minuscule.

---
+++ scripts/content-script.js
@@ -0,0 +1,36 @@
+// Parcours les éléments de la page HTML
+function trouverMot(listeMots, parent = document.body) {
+  // Exclure les éléments avec le tag "a" et leurs enfants
+  if (parent.tagName === 'A') {
+    return;
+  }
+
+  const enfants = parent.childNodes;
+  for (const enfant of enfants) {
+    if (enfant.nodeType === Node.TEXT_NODE) {
+      const contenuElement = enfant.textContent.toLowerCase();
+      for (const mot of listeMots) {
+        if (contenuElement.includes(mot.toLowerCase())) {
+          surlignage(mot, contenuElement, enfant, parent);
+        }
+      }
+    } else if (enfant.nodeType === Node.ELEMENT_NODE) {
+      // Appeler la fonction de manière récursive sur l'enfant si ce n'est pas un lien
+      if (enfant.tagName !== 'A') {
+        trouverMot(listeMots, enfant);
+      }
+    }
+  }
+}
+
+// Surligne le mot recherché
+function surlignage(mot, contenuElement, enfant, parent) {
+  
+    const nouveauContenu = enfant.textContent.replace(new RegExp(`\\b(${mot})\\b`, 'gi'), `<span style="background-color: yellow;">$1</span>`);
+    const nouvelEnfant = document.createElement('span');
+    nouvelEnfant.innerHTML = nouveauContenu;
+    parent.replaceChild(nouvelEnfant, enfant);
+  
+}
+
+setTimeout(() => trouverMot(["Macron"]), 2000);
--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,16 @@
-const getData = async (url) => {
-    const response = await fetch(url);
+// const getData = async (url) => {
+//     const response = await fetch(url);
 
-    if (response.status != 200) {
-        throw new Error("Cannot fetch the data.");
-    };
+//     if (response.status != 200) {
+//         throw new Error("Cannot fetch the data.");
+//     };
 
-    const data = await response.json();
-    return data;
-}
+//     const data = await response.json();
+//     return data;
+// }
 
-getData("donnee.json")
-    .then(data => {
+// getData("donnee.json")
+//     .then(data => {
 
-    })
+//     })
 
-function trouverMot(listeMots) {
-    const elementsPage = document.body.querySelectorAll('*:not(script)');
-    for (const element of elementsPage) {
-        console.log(element)
-        const contenuElement = element.textContent.toLowerCase();
-        for (const mot of listeMots) {
-            if (contenuElement.includes(mot.toLowerCase())) {
-                const nouveauContenu = contenuElement.replace(mot, `<span style="background-color: yellow;">${mot}</span>`)
-                element.innerHTML = nouveauContenu;
-                return
-            }
-        }
-    }
-}
