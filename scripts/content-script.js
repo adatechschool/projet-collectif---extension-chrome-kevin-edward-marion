68a6a8a42cc060d06168a959a9e9863080b1aba9
Kévin C <kevin.chron@gmail.com>
2023-05-09 16:51:43 +0200

    Ajout bulle quand mousehover

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,4 +1,6 @@
-chrome.runtime.sendMessage({ type: 'getData' }, response => {
+
+
+chrome.runtime.sendMessage({ type: 'getData' },  response => {
   console.log(response);
   let listeNoms = [];
   let listePrenoms = [];
@@ -7,8 +9,41 @@ chrome.runtime.sendMessage({ type: 'getData' }, response => {
     listePrenoms.push(politicien["prénom"]);
   }
   console.log(listeNoms);
-  setTimeout(() => trouverMot(listeNoms), 2000)
+
+  setTimeout(() => {
+    trouverMot(listeNoms);
+    let poli = document.querySelectorAll('.politicien');
+    let text = document.createElement('div');
+    text.style.position = 'absolute';
+    text.style.background = 'black';
+    text.style.padding = '10px';
+    text.style.borderRadius = '15px';
+    text.style.display = 'none';
+    document.body.appendChild(text);
+    text.innerText = "Bonjour";
+    text.style.color= "white";
+  
+    poli.forEach(nom => {
+      nom.addEventListener("mouseenter", (e) => {
+        text.style.display = 'block';
+        positionnerTexte(text, e.clientX, e.clientY);
+      });
+      nom.addEventListener("mousemove", (e) => {
+        positionnerTexte(text, e.clientX, e.clientY);
+      });
+      nom.addEventListener("mouseleave", (e) => {
+        text.style.display = 'none';
+      });
+    })
+  }, 2000);
+  
+  
 });
+
+function positionnerTexte(element, x, y) {
+  element.style.left = x + 10 + 'px';
+  element.style.top = y + 10 + 'px';
+}
 
 // Parcourir les éléments de la page HTML
 function trouverMot(listeMots, parent = document.body) {
@@ -19,22 +54,7 @@ function trouverMot(listeMots, parent = document.body) {
   const enfants = parent.childNodes;
   for (const enfant of enfants) {
     if (enfant.nodeType === Node.TEXT_NODE) {
-      const contenuElementLower = enfant.textContent.toLowerCase();
-      const contenuElement = enfant.textContent;
-      let nouveauContenu = contenuElement;
-      for (const mot of listeMots) {
-        if (contenuElementLower.includes(mot.toLowerCase())) {
-          nouveauContenu = nouveauContenu.replace(
-            new RegExp(`(${mot})`, 'gi'),
-            '<span style="background-color: yellow;">$1</span>'
-          );
-        }
-      }
-      if (contenuElementLower !== nouveauContenu.toLowerCase()) {
-        const nouvelEnfant = document.createElement('span');
-        nouvelEnfant.innerHTML = nouveauContenu;
-        parent.replaceChild(nouvelEnfant, enfant);
-      }
+      surlignage(listeMots, enfant, parent)
     } else if (enfant.nodeType === Node.ELEMENT_NODE) {
       if (enfant.tagName !== 'A') {
         trouverMot(listeMots, enfant);
@@ -43,15 +63,23 @@ function trouverMot(listeMots, parent = document.body) {
   }
 }
 
-// setTimeout(() => trouverMot(["Macron", "Borne"]), 2000);
-
 
 // Surligner le mot recherché
-// function surlignage(mot, enfant, parent) {
-  
-//     const nouveauContenu = enfant.textContent.replace(mot, `<span style="background-color: yellow;">${mot}</span>`);
-//     const nouvelEnfant = document.createElement('span');
-//     nouvelEnfant.innerHTML = nouveauContenu;
-//     parent.replaceChild(nouvelEnfant, enfant);
-  
-// }
+function surlignage(listeMots, enfant, parent) {
+  const contenuElementLower = enfant.textContent.toLowerCase();
+  const contenuElement = enfant.textContent;
+  let nouveauContenu = contenuElement;
+  for (const mot of listeMots) {
+    if (contenuElementLower.includes(mot.toLowerCase())) {
+      nouveauContenu = nouveauContenu.replace(
+        new RegExp(`(${mot})`, 'gi'),
+        '<span style="background-color: rgb(204 255 255); color:black;" class="politicien">$1</span>'
+      );
+    }
+  }
+  if (contenuElementLower !== nouveauContenu.toLowerCase()) {
+    const nouvelEnfant = document.createElement('span');
+    nouvelEnfant.innerHTML = nouveauContenu;
+    parent.replaceChild(nouvelEnfant, enfant);
+  }
+}
