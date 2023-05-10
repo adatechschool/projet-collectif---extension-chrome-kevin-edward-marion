6929333635c07d6baa32d7812bef315ec8d5b04f
Kévin C <kevin.chron@gmail.com>
2023-05-10 12:06:55 +0200

    Retour de la bonne version du service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,24 @@
- const getData = async (url) => {
-    console.log(url);
-    const response = await fetch(url);
-  
-    if (response.status != 200) {
-        throw new Error("Cannot fetch the data.");
-    };
-  
-    const data = await response.json();
-    return data;
-  }
-  
-  getData("../gouvernement.json")
-    .then(data => {
-      console.log(data)
-    })
-    .catch(error => {
-        console.error(error);
- 
+let politiciens;
 
-    })
+async function fetchData() {
+  const response = await fetch("../gouvernement.json");
+  const data = await response.json();
+  politiciens = data;
+}
+
+self.addEventListener('install', event => {
+  event.waitUntil(fetchData());
+});
+
+self.addEventListener('message', event => {
+  if (event.data.type === 'getData') {
+    event.source.postMessage({ type: 'data', data: politiciens });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'getData') {
+    sendResponse({ message: 'Data received.', data: politiciens });
+    
+  }
+});
